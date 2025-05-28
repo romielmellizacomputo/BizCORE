ad43cfd566cfddae75e448ca16fc2d68fb76ba04
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:36:58 +0800

    Update pull_expenses.py

--- services/pull_expenses.py
+++ services/pull_expenses.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_expenses_data():
     service = authenticate_google_sheets()
-    range_name = "Expenses!G4:T"
+    range_name = "Expenses!G4:U"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
