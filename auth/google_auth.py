92a5b80d87594d4981cce4f41b10ce058246b913
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 03:37:24 +0800

    Update google_auth.py

--- auth/google_auth.py
+++ auth/google_auth.py
@@ -1,13 +1,13 @@
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
 import os
 import json
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
 def get_sheets_service():
-    # Get JSON content from environment variable (GitHub secret)
-    service_account_info = json.loads(os.environ['BIZNEST_AGENT'])
-    credentials = service_account.Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
-    service = build('sheets', 'v4', credentials=credentials)
+    creds = service_account.Credentials.from_service_account_info(
+        json.loads(os.environ['BIZNEST_AGENT']),
+        scopes=["https://www.googleapis.com/auth/spreadsheets.readonly"]
+    )
+    service = build('sheets', 'v4', credentials=creds)
     return service
