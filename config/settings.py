5c3b1e42d419374b5ac8d8dfcdcdf6878a67b6b1
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 03:08:33 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,18 +1,3 @@
-# File: config/settings.py
-import os
-
-CORE_SHEET_ID = os.getenv("CORE")
-CORE_HANDLER_ID = os.getenv("CORE_HANDLER")
-
-PERMISSIONS_SHEET_MAP = {
-    "Products": "pull_products",
-    "Sales": "pull_revenue",
-    "Procurements": "pull_procurements",
-    "Expenses": "pull_expenses",
-    "Suppliers": "pull_suppliers",
-    "Resellers": "pull_resellers",
-    "Investments": "pull_investments",
-    "Cash-Flow": "pull_transactions",
-    "Business Meetings": "pull_meetings",
-    "Business Goals": "pull_goals"
-}
+# config/settings.py
+CORE_SHEET_ID = 'your_core_sheet_id'
+CORE_HANDLER_SHEET_ID = 'your_core_handler_sheet_id'
