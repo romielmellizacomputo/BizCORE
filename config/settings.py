c5b7e37bec6b8fc4a0bb2970254bc07d525b7d54
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 03:18:06 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,21 @@
-# config/settings.py
-CORE_SHEET_ID = 'your_core_sheet_id'
-CORE_HANDLER_SHEET_ID = 'your_core_handler_sheet_id'
+import os
+
+CORE_SHEET_ID = os.getenv('CORE')              # Main CORE sheet ID
+CORE_HANDLER_SHEET_ID = os.getenv('CORE_HANDLER')  # Sheet with children IDs and permissions
+
+# Sheets to handle
+SHEETS = {
+    "Products": "G:U",
+    "Sales": "G:T",
+    "Procurements": "G:V",
+    "Expenses": "G:T",
+    "Suppliers": "G:R",
+    "Resellers": "G:R",
+    "Investments": "G:S",
+    "Cash-Flow": "G:N",
+    "Business Meetings": "G:N",
+    "Business Goals": "G:N",
+}
+
+START_ROW = 4  # data start row in core sheets
+TARGET_START_ROW = 11  # target sheets clear & insert start row
