29f7f2e0007c3901cb80e460b41fd06f62e516a8
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:09:47 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,8 @@
+import os
+
 CORE_SHEET_ID = os.environ['CORE']
 CORE_HANDLER_SHEET_ID = os.environ['CORE_HANDLER']
+
 SHEET_NAMES = [
     "Products", "Sales", "Procurements", "Expenses", "Suppliers",
     "Resellers", "Investments", "Cash-Flow", "Business Meetings", "Business Goals"
