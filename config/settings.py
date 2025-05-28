9a7e0c69365c61b31dc71fea4722763e0844e79a
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 03:43:29 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,21 +1,22 @@
+# ============================
+# config/settings.py
+# ============================
 import os
 
-CORE_SHEET_ID = os.getenv('CORE')              # Main CORE sheet ID
-CORE_HANDLER_SHEET_ID = os.getenv('CORE_HANDLER')  # Sheet with children IDs and permissions
+CORE_ID = os.getenv("CORE")
+CORE_HANDLER_ID = os.getenv("CORE_HANDLER")
 
-# Sheets to handle
-SHEETS = {
-    "Products": "G:U",
-    "Sales": "G:T",
-    "Procurements": "G:V",
-    "Expenses": "G:T",
-    "Suppliers": "G:R",
-    "Resellers": "G:R",
-    "Investments": "G:S",
-    "Cash-Flow": "G:N",
-    "Business Meetings": "G:N",
-    "Business Goals": "G:N",
+SHEET_MAPPINGS = {
+    "Products": ("G", "U"),
+    "Sales": ("G", "T"),
+    "Procurements": ("G", "V"),
+    "Expenses": ("G", "T"),
+    "Suppliers": ("G", "R"),
+    "Resellers": ("G", "R"),
+    "Investments": ("G", "S"),
+    "Cash-Flow": ("G", "N"),
+    "Business Meetings": ("G", "N"),
+    "Business Goals": ("G", "N")
 }
 
-START_ROW = 4  # data start row in core sheets
-TARGET_START_ROW = 11  # target sheets clear & insert start row
+ALL_SHEETS = list(SHEET_MAPPINGS.keys())
