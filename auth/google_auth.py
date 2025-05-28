d47f336cec4ad615b17915ef8b497ac1b94676de
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 02:38:56 +0800

    Update google_auth.py

--- auth/google_auth.py
+++ auth/google_auth.py
@@ -1,1 +1,12 @@
+# File: auth/google_auth.py
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+import os
 
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+
+def get_google_sheets_service():
+    credentials = service_account.Credentials.from_service_account_info(
+        eval(os.environ['BIZNEST_AGENT']), scopes=SCOPES
+    )
+    return build('sheets', 'v4', credentials=credentials).spreadsheets()
