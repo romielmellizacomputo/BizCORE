6324d241d4aef690f6e664b274aa587ce772411d
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:34:54 +0800

    Update pull_products.py

--- services/pull_products.py
+++ services/pull_products.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_products_data():
     service = authenticate_google_sheets()
-    range_name = "Products!G4:U"
+    range_name = "Products!G4:V"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
