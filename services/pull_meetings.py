8aa44946ec74b0384e32df7978f36aceb7033f27
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:55:13 +0800

    Update pull_meetings.py

--- services/pull_meetings.py
+++ services/pull_meetings.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_meetings_data():
     service = authenticate_google_sheets()
-    range_name = "Business Meetings!G4:O"
+    range_name = "Business Meetings!G4:N"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
