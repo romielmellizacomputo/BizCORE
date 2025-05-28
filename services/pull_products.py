41426069f2bc589be9a62737a0f4fd6a5704e9ad
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 03:51:08 +0800

    Update pull_products.py

--- services/pull_products.py
+++ services/pull_products.py
@@ -1,6 +1,10 @@
 # ============================
 # services/pull_products.py (Similar for other pull_*.py)
 # ============================
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from services.base_puller import fetch_and_push_data
 
 def main():
