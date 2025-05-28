dd42d0729ed84a5934b367a8bf615a62fd2fa8f6
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:36:18 +0800

    Update pull_procurements.py

--- services/pull_procurements.py
+++ services/pull_procurements.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_procurements_data():
     service = authenticate_google_sheets()
-    range_name = "Procurements!G4:V"
+    range_name = "Procurements!G4:W"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
