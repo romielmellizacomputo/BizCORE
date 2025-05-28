d7ed48002abb10dc681d24a6f6e85179cbae302d
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:36:48 +0800

    Update pull_goals.py

--- services/pull_goals.py
+++ services/pull_goals.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_goals_data():
     service = authenticate_google_sheets()
-    range_name = "Business Goals!G4:N"
+    range_name = "Business Goals!G4:O"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
