e48936b8e5a361d31dc9a0d162b928e34fc66095
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:35:58 +0800

    Update pull_suppliers.py

--- services/pull_suppliers.py
+++ services/pull_suppliers.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_suppliers_data():
     service = authenticate_google_sheets()
-    range_name = "Suppliers!G4:R"
+    range_name = "Suppliers!G4:S"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
