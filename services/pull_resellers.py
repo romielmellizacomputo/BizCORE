af596e36ef52c2faff8cd11a6b1f0f9f04fe0a1d
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:35:32 +0800

    Update pull_resellers.py

--- services/pull_resellers.py
+++ services/pull_resellers.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_resellers_data():
     service = authenticate_google_sheets()
-    range_name = "Resellers!G4:R"
+    range_name = "Resellers!G4:S"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
