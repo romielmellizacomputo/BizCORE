d099ea7ce07bbed4a6653284401d936e5bb0e8f0
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 04:55:23 +0800

    Update pull_investments.py

--- services/pull_investments.py
+++ services/pull_investments.py
@@ -3,7 +3,7 @@ from config.settings import CORE_SHEET_ID
 
 def fetch_investments_data():
     service = authenticate_google_sheets()
-    range_name = "Investments!G4:T"
+    range_name = "Investments!G4:S"
     result = service.spreadsheets().values().get(
         spreadsheetId=CORE_SHEET_ID, range=range_name
     ).execute()
