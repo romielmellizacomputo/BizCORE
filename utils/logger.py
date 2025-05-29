88b8623be988a538bd19940450061a7cd3bdf8a1
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-30 01:40:40 +0800

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,11 @@
+def get_logger(name):
+    import logging
+    logger = logging.getLogger(name)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
 
