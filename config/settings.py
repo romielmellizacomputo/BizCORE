17fdd5cd674797473c4cf521f204d4b691592226
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-29 23:09:39 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,21 @@
 import os
 
-CORE_SHEET_ID = os.environ['CORE']
-CORE_HANDLER_SHEET_ID = os.environ['CORE_HANDLER']
+CORE_SHEET_ID = os.getenv("CORE")
+CORE_HANDLER_SHEET_ID = os.getenv("CORE_HANDLER")
 
-SHEET_NAMES = [
-    "Products", "Sales", "Procurements", "Expenses", "Suppliers",
-    "Resellers", "Investments", "Cash-Flow", "Business Meetings", "Business Goals"
-]
+SHEET_RANGES = {
+    "Products": "G:O",
+    "Sales": "G:O",
+    "Procurements": "G:R",
+    "Expenses": "G:R",
+    "Suppliers": "G:Q",
+    "Resellers": "G:O",
+    "Investments": "G:P",
+    "Cash-Flow": "G:L",
+    "Business Meetings": "G:N",
+    "Business Goals": "G:N"
+}
+
+INSERT_START_ROW = 11
+SOURCE_BUSINESS_ID_COL = "D"
+SOURCE_LABEL_COL = "B"
