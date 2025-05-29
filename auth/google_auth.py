f814bcf5613d2b1f3dedb2f1e2668c7d655dbf91
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-30 00:06:53 +0800

    Update google_auth.py

--- auth/google_auth.py
+++ auth/google_auth.py
@@ -1,8 +1,11 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+import os
+import json
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
-def authenticate_google_sheets():
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("path/to/your/service_account.json", scope)
-    client = gspread.authorize(creds)
-    return client
+def get_gspread_service():
+    service_account_info = os.environ['BIZNEST_AGENT']
+    creds_info = json.loads(service_account_info)
+    credentials = service_account.Credentials.from_service_account_info(creds_info, scopes=["https://www.googleapis.com/auth/spreadsheets"])
+    service = build('sheets', 'v4', credentials=credentials)
+    return service
