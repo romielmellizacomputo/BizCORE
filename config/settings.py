f217e8870d53773205ea61a4bb18e0c212911d63
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-30 00:15:34 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,20 @@
+# --- config/settings.py ---
 import os
 
-CORE = os.environ.get('CORE')  # Google Sheet ID for your main database
-CORE_HANDLER = os.environ.get('CORE_HANDLER')  # Google Sheet ID for business sheets list
+CORE_SHEET_ID = os.environ.get("CORE")
+CORE_HANDLER_SHEET_ID = os.environ.get("CORE_HANDLER")
+
+PERMISSION_SHEET_MAP = {
+    "Products": ("Products", "G", "O"),
+    "Sales": ("Sales", "G", "O"),
+    "Procurements": ("Procurements", "G", "R"),
+    "Expenses": ("Expenses", "G", "R"),
+    "Suppliers": ("Suppliers", "G", "Q"),
+    "Resellers": ("Resellers", "G", "O"),
+    "Investments": ("Investments", "G", "P"),
+    "Cash-Flow": ("Cash-Flow", "G", "L"),
+    "Business Meetings": ("Business Meetings", "G", "N"),
+    "Business Goals": ("Business Goals", "G", "N")
+}
+
+ALL_PERMISSIONS = list(PERMISSION_SHEET_MAP.keys())
