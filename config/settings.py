ff3db3e08247d2b5b0299007ec87d2bff1bef2ce
Romiel Melliza Computo <romielmellizacomputo@gmail.com>
2025-05-31 08:03:11 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -5,16 +5,15 @@ CORE_SHEET_ID = os.environ.get("CORE")
 CORE_HANDLER_SHEET_ID = os.environ.get("CORE_HANDLER")
 
 PERMISSION_SHEET_MAP = {
-    "Products": ("Products", "G", "O"),
-    "Sales": ("Sales", "G", "O"),
-    "Procurements": ("Procurements", "G", "R"),
-    "Expenses": ("Expenses", "G", "R"),
-    "Suppliers": ("Suppliers", "G", "Q"),
-    "Resellers": ("Resellers", "G", "O"),
-    "Investments": ("Investments", "G", "P"),
-    "Cash-Flow": ("Cash-Flow", "G", "L"),
-    "Business Meetings": ("Business Meetings", "G", "N"),
-    "Business Goals": ("Business Goals", "G", "N")
+    "Products": ("Products", "G", "R"),         
+    "Sales": ("Sales", "G", "W"),             
+    "Expenses": ("Expenses", "G", "R"),       
+    "Suppliers": ("Suppliers", "G", "R"),     
+    "Sellers": ("Sellers", "G", "S"),          
+    "Investments": ("Investments", "G", "T"), 
+    "Cash-Flow": ("Cash-Flow", "G", "N"),      
+    "Business Meetings": ("Business Meetings", "G", "O"),  
+    "Business Goals": ("Business Goals", "G", "O")         
 }
 
 ALL_PERMISSIONS = list(PERMISSION_SHEET_MAP.keys())
